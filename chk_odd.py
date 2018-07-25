c48856ab5a497e71f3e2c5e36577dee05531c2ff
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-25 19:10:28 +0530

    Update and rename README.md to chk_odd.py

---
+++ chk_odd.py
@@ -0,0 +1,12 @@
+# chk_odd
+def main():
+    pass
+    n=list(map(int,input()))
+    for i in n:
+        if(i%2==0):
+            pass
+        else:
+            print(i,end=" ")
+
+if __name__ == '__main__':
+    main()
